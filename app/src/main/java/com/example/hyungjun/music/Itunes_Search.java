41c248f0b5d307f04adacaf6b4658636817942f0
Lee Min Haeng <littlecold2@naver.com>
2018-08-04 18:18:22 +0900

    yes: list 받아오기, 음악리스트 바꾸기, no: 리스트 클릭시 재생, 플레이 커버 바꾸기

---
+++ app/src/main/java/com/example/hyungjun/music/Itunes_Search.java
@@ -0,0 +1,4 @@
+package com.example.hyungjun.music;
+
+public class Itunes_search {
+}
---
+++ app/src/main/java/com/example/hyungjun/music/Music_Data.java
@@ -0,0 +1,28 @@
+package org.androidtown.ituens_api_search_test;
+
+/**
+ * Created by MIN on 2018-06-09.
+ */
+
+public class Music_Data {
+
+
+
+//    int artist_id;
+    public String title;
+    public String singer;
+    public String album;
+//    int ranking;
+
+
+    Music_Data(String title, String singer, String album)
+    {
+      this.title = title;
+      this.singer = singer;
+      this.album = album;
+    }
+
+
+
+
+}
